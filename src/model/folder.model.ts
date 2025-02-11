99c09832db98e15aa0fd9c964de540d9c45982f4
nusratalam123 <nusratalam975@gmail.com>
2025-02-11 14:41:46 +0600

    update folder model

--- src/model/folder.model.ts
+++ src/model/folder.model.ts
@@ -5,6 +5,8 @@ interface IFolder extends Document {
   name: string;
   parentFolder?: mongoose.Schema.Types.ObjectId | null;
   createdAt: Date;
+  size: number; 
+  favorite: boolean;
 }
 
 const FolderSchema = new Schema<IFolder>({
@@ -12,6 +14,23 @@ const FolderSchema = new Schema<IFolder>({
   name: { type: String, required: true },
   parentFolder: { type: Schema.Types.ObjectId, ref: "Folder", default: null },
   createdAt: { type: Date, default: Date.now },
+  size: { type: Number, default: 0 },
+  favorite: { type: Boolean, default: false },
+});
+
+// Hook to update the user's total storage size when a file is added
+FolderSchema.pre("save", async function (next) {
+  const folder = this;
+
+  if (folder.isModified("size")) {
+    const user = await mongoose.model("User").findById(folder.userId);
+    if (user) {
+      user.totalSize -= folder.size; 
+      await user.save();
+    }
+  }
+
+  next();
 });
 
 export default mongoose.model<IFolder>("Folder", FolderSchema);
